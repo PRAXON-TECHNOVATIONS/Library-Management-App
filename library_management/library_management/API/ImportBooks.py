10a4e1d57f0eaeda73ace67fc08861fb0c3ea5be
Yankyyyy <y.mangshatabam@leadergroup.com>
2022-04-07 14:44:15 +0530

    import books update

--- library_management/library_management/API/ImportBooks.py
+++ library_management/library_management/API/ImportBooks.py
@@ -6,25 +6,28 @@ import json
 def createArticles():
     data = json.loads(frappe.request.data)
     frappe.msgprint(data)
-    if not frappe.db.exists('Article', {'isbn': data.get('isbn')}):
-        article = frappe.new_doc('Article')
-        article.isbn = data.get('isbn')
-    else:
-        article = frappe.get_doc('Article', {'isbn': data.get('isbn')})
-    if data.get('title'):
-        article.title = data.get('title')
-    if data.get('author'):
-        article.author = data.get('author')
-    if data.get('isbn'):
-        article.isbn = data.get('isbn')
-    if data.get('publisher'):
-        article.publisher = data.get('publisher')
-    if data.get('page'):
-        article.page = data.get('page')
-    if data.get('total_quantity'):
-        article.total_quantity = data.get('total_quantity')
-    if data.get('language'):
-        article.language = data.get('language')
-    article.stock = data.get('total_quantity')
-    article.save(ignore_permissions=True)
-    frappe.db.commit()
+    if data.get("books"):
+        books = data.get("books")
+        for book in books:
+            if not frappe.db.exists('Article', {'isbn': book.get('isbn')}):
+                article = frappe.new_doc('Article')
+                article.isbn = book.get('isbn')
+            else:
+                article = frappe.get_doc('Article', {'isbn': book.get('isbn')})
+            if book.get('title'):
+                article.title = book.get('title')
+            if book.get('author'):
+                article.author = book.get('author')
+            if book.get('isbn'):
+                article.isbn = book.get('isbn')
+            if book.get('publisher'):
+                article.publisher = book.get('publisher')
+            if book.get('page'):
+                article.page = book.get('page')
+            if book.get('total_quantity'):
+                article.total_quantity = book.get('total_quantity')
+            if book.get('language'):
+                article.language = book.get('language')
+            article.stock = book.get('total_quantity')
+            article.save(ignore_permissions=True)
+            frappe.db.commit()
